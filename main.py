25ba58b956afdf22bc10164bd62d2a740837437f
hani hadddad <henihaddad@insat.u-carthage.tn>
2023-09-14 15:32:24 +0100

    rename

--- main.py
+++
@@ -1,6 +0,0 @@
-from Converter import Converter
-converter=Converter(r".\Aspose.Total.NET.lic")
-converter.convert_to_pdf("./examples/mybestdocument.docx", "./examples/mybestdocument.pdf" )
-
-
-converter.convert_to_pdf("./examples/9anoungypt.pptx", "./examples/9anoungypt.pdf" )
